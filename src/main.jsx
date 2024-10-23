21e3d24522ac830e12c346d6f92d63a9cf9d1067
Muna <muna.abdi@gritacademy.se>
2024-10-23 04:23:56 +0200

    muna

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './ App'; // Huvudkomponenten för din app
+import App from './ app'; // Huvudkomponenten för din app
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
